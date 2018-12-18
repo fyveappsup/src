519cfa611aedd8aa87c9737e173a195384c5b761
TheoStJoSup <theo.guil76@outlook.fr>
2018-12-18 10:53:57 +0100

    plus besoin de se co si deja fait

--- Fyve/src/app/app.component.ts
+++ Fyve/src/app/app.component.ts
@@ -2,15 +2,23 @@ import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { Storage } from '@ionic/storage';
 
 import { HomePage } from '../pages/home/home';
+import {MenuPage } from '../pages/menu/menu';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+  // si deja connecter et valeur enregistrer pas besoin de s'authentifier
   rootPage:any = HomePage;
 
-  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+  constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, public storage: Storage) {
+    this.storage.length().then((val) => {
+      if(val>5){
+        this.rootPage = MenuPage;
+      }
+    });
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
--- Fyve/src/pages/myprofil/myprofil.ts
+++ Fyve/src/pages/myprofil/myprofil.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FirebaseProvider } from './../../providers/firebase/firebase';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-myprofil',
@@ -7,8 +9,30 @@ import { NavController } from 'ionic-angular';
 })
 export class MyProfilPage {
 
-  constructor(public navCtrl: NavController) {
+  // Donnée de l'utilisateur
+  id = '';
+  nom = '';
+  prenom = '';
+  mail = '';
+  mdp ='';
+  description = '';
+  serveur : boolean;
 
+  isModifiable=false;
+
+
+  constructor(public nav: NavController, public storage : Storage, public firebaseProvider: FirebaseProvider ) {
+    this.storage.get("id").then((val) => {this.id=val;});
+    this.storage.get("prenom").then((val) => {this.prenom=val;});
+    this.storage.get("nom").then((val) => {this.nom= val;});
+    this.storage.get("description").then((val) => {this.description=val;});
+    this.storage.get("mdp").then((val) => {this.mdp=val;});
+    this.storage.get("mail").then((val) => {this.mail=val;});
+    this.storage.get("serveur").then((val) => {this.serveur=val;});
+  }
+
+  public modifier(){
+   this.isModifiable=true;
   }
 
 }
