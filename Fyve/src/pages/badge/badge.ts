d2e1ab9c244993d766be2055e5b958620c43435f
TheoG <t.guillemard@stjosup.com>
2018-12-27 16:04:02 +0100

    badge

--- Fyve/src/pages/badge/badge.ts
+++ Fyve/src/pages/badge/badge.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { FirebaseProvider } from './../../providers/firebase/firebase';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'page-badge',
@@ -7,8 +9,41 @@ import { NavController } from 'ionic-angular';
 })
 export class BadgePage {
 
-  constructor(public navCtrl: NavController) {
+  section = "sommes";
 
+  listPaiementDonnes = null;
+  lesPaiementsDonnes = [];
+  totalDonnes = 0;
+
+  idUser = null;
+
+  constructor(public navCtrl: NavController, public firebaseProvider: FirebaseProvider, public storage: Storage) {
+    
+  }
+
+  ionViewWillEnter(){
+    this.storage.get("id").then((val) => {
+      this.idUser=val;
+      console.log("idUser", this.idUser);
+
+      // paiemments données
+      this.listPaiementDonnes = this.firebaseProvider.getPaiementDonnesById(this.idUser);
+      this.listPaiementDonnes.forEach(element => {
+        this.lesPaiementsDonnes=[];
+        this.lesPaiementsDonnes=element;
+        this.getTotalDonnes();
+      });
+      console.log("paiement donnes",this.lesPaiementsDonnes);
+
+    });
+  }
+
+  public getTotalDonnes(){
+    this.totalDonnes=0;
+    this.lesPaiementsDonnes.forEach(item =>{
+      console.log("item", item);
+      this.totalDonnes+=item.montant;
+    });
   }
 
 }
